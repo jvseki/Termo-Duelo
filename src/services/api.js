a74f96a2d304737d5244f539d0899e7958833f71
jvseki <jv.s.m1007@gmail.com>
2025-10-13 00:16:55 -0300

    api auxios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // depois aqui vai o endereço do back-end
+  baseURL: "http://localhost:3000", // endereço do back-end
 });
 
 export default api;
